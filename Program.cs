930181c74e78b26b0f8150ab23dc56e0d056f1dc
Felipe França <contatofelipefranca12@gmail.com>
2020-09-28 11:57:25 -0300

    Resolução do exercício VelocMedia

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("\n ---- Velocidade média ----");
+            Console.Write("\n Digite a distância percorrida (m): ");
+            double distanciaPercorrida = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n Digite o tempo gasto (s) : ");
+            double tempoGasto = Convert.ToDouble(Console.ReadLine());
+
+            double velocidadeMedia = distanciaPercorrida / tempoGasto;
+            Console.WriteLine($"\n Velocidade média: {velocidadeMedia}");
+            Console.Write("\n Pressione qualquer tecla para sair");
+            Console.ReadKey();
+            
+        }
+    }
+}
